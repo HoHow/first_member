0cf390790d1cf48d4fb0c291cf360924370cc650
wuhoho <wumingho@gmail.com>
2017-03-27 18:03:23 +0800

    delete parse

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,6 @@ var express   = require('express');
 var bodyParser = require('body-parser');
 var knex      = require('../knexfile');
 var router    = express.Router();
-router.use(bodyParser.json()); // for parsing application/json
-router.use(bodyParser.urlencoded({ extended: true }));
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
